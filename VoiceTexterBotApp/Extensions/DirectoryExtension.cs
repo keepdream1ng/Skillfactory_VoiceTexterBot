dca07b3cbc2ccda04fe0ff056a6f2d50a5b0761b
Anatolii Ivanov <gotnickname@gmail.com>
2023-05-01 10:42:07 +0530

    added extensions classes

---
+++ VoiceTexterBotApp/Extensions/DirectoryExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace VoiceTexterBotApp.Extensions
+{
+    public static class DirectoryExtension
+    {
+        /// <summary>
+        /// Getting path to the solution folder.
+        /// </summary>
+        public static string GetSolutionRoot()
+        {
+            var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            var fullname = Directory.GetParent(dir).FullName;
+            var projectRoot = fullname.Substring(0, fullname.Length - 4);
+            return Directory.GetParent(projectRoot)?.FullName;
+        }
+    }
+}
---
+++ VoiceTexterBotApp/Extensions/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace VoiceTexterBotApp.Extensions
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Converting string, so it will start crom uppercase letter. 
+        /// </summary>
+        public static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+    }
+}
